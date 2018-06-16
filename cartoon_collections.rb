57f9a9e3b7bbedd1d37f1a9e6510cef0c20e1524
ryhan.chowdhury3@gmail.com <ryhan.chowdhury3@gmail.com>
2018-06-16 19:16:01 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,8 @@ end
 def summon_captain_planet(planeteers)# code an argument here
   # Your code here
   planeteers_changed = planeteers.collect do |planeteer|
-    planeteer
+    planeteer_capitalized = planeteer.capitalize
+    
 end
 
 def long_planeteer_calls# code an argument here
