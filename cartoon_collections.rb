0bcd4eee1a2cb7da9c11ccf002c230724f0ae7df
ryhan.chowdhury3@gmail.com <ryhan.chowdhury3@gmail.com>
2018-06-16 19:24:58 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,6 +25,9 @@ end
 def find_the_cheese(cheeses)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_type.each do |cheese|
+  cheese_types.each do |cheese_type|
+    cheeses.each do |cheese|
+      if cheese_type = cheese 
+        return cheese_type
     
 end
